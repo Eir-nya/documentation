d5622ce8cd537cbfa1df59260d7584e0d3498e7a
WD200019 <warrend2000@gmail.com>
2019-10-02 17:58:09 -0500

    fix previous thing for real + tried to fix lists for MS Edge

--- js/pages.js
+++ js/pages.js
@@ -677,7 +677,7 @@ else {
 var links = document.getElementsByTagName("a");
 
 for (var i = 0; i < links.length; i++) {
-    if (links[i].getAttribute("href") != null && links[i].href.indexOf("http") == 0) {
+    if (links[i].getAttribute("href") != null && links[i].getAttribute("href").indexOf("http") == 0) {
         links[i].className = "external";
         links[i].setAttribute("title", "External link: " + links[i].href);
     };
